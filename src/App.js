730eefd4d514507c719adf1c7738e3534dab9704
KimSeungHu <mind9063@kaist.ac.kr>
2019-05-27 16:05:28 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { hot } from 'react-hot-loader';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <h1>asdfads</h1>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
